810047cbd454155b4fda22bf5106f186a5df49e0
Miles Higbie <miles.higbie@gmail.com>
2020-01-14 00:32:57 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,30 +1,30 @@
-class BankAccount
+require "pry"
 
-attr_accessor :balance, :status
-attr_reader :name
-
+class BankAccount 
+  
+  attr_reader :name 
+  attr_accessor :balance, :status
+  
   def initialize(name)
-    @name = name
-    @balance = 1000
+    @name = name 
+    @balance = 1000 
     @status = "open"
   end 
   
-  def deposit(deposit_amount) 
-    @balance += deposit_amount
+  def deposit(amount) 
+    @balance += amount
   end 
   
-  def display_balance
-    return "Your balance is $#{@balance}."
+  def display_balance 
+    "Your balance is $#{@balance}."
   end 
   
   def valid?
-  @status == "open" && @balance > 0 ? true : false 
+    self.status == "open" && self.balance > 0 
   end 
   
   def close_account 
-    @status = "closed"
+    self.status = "closed"
   end 
   
-  
-
-end
+end 
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,6 @@
 require "pry"
 
-
-class Transfer
+class Transfer 
   
   attr_accessor :sender, :receiver, :status, :amount
   
@@ -12,32 +11,29 @@ class Transfer
     @amount = amount
   end 
   
-  # def valid?
-  #   if @sender
-  #   @receiver
-  # end 
-  
   def valid? 
-    @sender.valid? && @receiver.valid?
+    sender.valid? && receiver.valid?
   end 
   
   def execute_transaction
-    if !self.valid? || @status == "complete" || @sender.balance < @amount 
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    else  
-      @sender.balance -= @amount 
+    if self.valid? && @status != "complete" && @amount < @sender.balance
+      @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
+    else 
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end 
   end 
   
-  def reverse_transfer 
+  def reverse_transfer
     if @status == "complete"
-      @sender.balance += @amount 
-      @receiver.balance -= @amount
-      @status = "reversed"
+       @sender.balance += @amount
+       @receiver.balance -= @amount
+       @status = "reversed"
     end 
   end 
   
-end
+  
+end 
+
