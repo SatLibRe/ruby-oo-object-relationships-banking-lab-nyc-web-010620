caa84d7b9f4cbadf8f13decc0f11b53a228e68fd
Miles Higbie <miles.higbie@gmail.com>
2020-01-14 00:14:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,11 +2,12 @@ require "pry"
 
 class Transfer 
   
-  attr_accessor :sender, :receiver, :status
+  attr_accessor :sender, :receiver, :status, :amount
   
-  def initialize(sender,receiver)
+  def initialize(sender,receiver,amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
+    @amount = amount
   end 
 end 
