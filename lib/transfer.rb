54ecb2c3ba87fde96467e83142ab54a962ebc57a
Miles Higbie <miles.higbie@gmail.com>
2020-01-14 00:25:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else 
-      
+      "Transaction rejected. Please check your account balance."
     end 
   end 
 end 
