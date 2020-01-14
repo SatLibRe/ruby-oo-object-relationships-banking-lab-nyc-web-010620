df2db4cf0620c0bb3aea6cd1bdf825df16501f75
Miles Higbie <miles.higbie@gmail.com>
2020-01-14 00:25:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if  @sender.valid? && @receiver.valid? || @status != "complete"
+    if  @sender.valid? && @receiver.valid? && @status != "complete"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
